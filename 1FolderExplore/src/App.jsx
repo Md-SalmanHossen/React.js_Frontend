f5fa5448d3eb276548474560c8b2214cfbf696c2
SalmanHossen <hossainsalman873@gmail.com>
2025-04-28 22:23:49 +0600

    explore folder structure

---
+++ 1FolderExplore/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-red-600 text-4xl font-extrabold p-4 text-center text-white">
+      this is react app.js file
+    </div>
+  )
+}
+
+export default App
