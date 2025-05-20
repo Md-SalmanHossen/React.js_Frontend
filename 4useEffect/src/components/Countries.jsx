c18ba08ccbf9e8ed90ad50496f83eb13eeb8bfc8
SalmanHossen <hossainsalman873@gmail.com>
2025-05-20 09:16:33 +0600

    conditional css practice

--- 4useEffect/src/components/Countries.jsx
+++ 4useEffect/src/components/Countries.jsx
@@ -4,6 +4,7 @@ import Country from "./Country";
 
 const Countries = () => {
    const [countries,setCountries]=useState([]);
+   const [visitedCountries,setVisitedCountries]=useState([]);
 
    useEffect(()=>{
       fetch('https://restcountries.com/v3.1/all')
@@ -12,15 +13,26 @@ const Countries = () => {
       
    },[])
 
-
+   const handleVisitedCountry=country=>{
+      console.log('add this is your visited country');
+      
+      console.log(country)
+   }
    return (
       <div >
          <h3 className="text-center">Countries : {countries.length}</h3>
+         <div>
+            <h4>Visited Country</h4>
+            <ul>
+
+            </ul>
+         </div>
          <div className="grid grid-cols-3">
             {
             countries.map(country=><Country 
                key={country.cca3} 
-               country={country}>
+               country={country}
+               handleVisitedCountry={handleVisitedCountry}>
             </Country>)
             }
          </div>
--- 4useEffect/src/components/Country.jsx
+++ 4useEffect/src/components/Country.jsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 
-const Country = ({country}) => {
-  if(!country) return <p>Loading country data......</p>
+const Country = ({ country, handleVisitedCountry }) => {
+  if (!country) return <p>Loading country data......</p>
 
-   const {name,flags,population,area,cca3}=country;
+  const { name, flags, population, area, cca3 } = country;
 
-   const[visited,setVisited]=useState(false);
-   const handleVisited=()=>{
-     setVisited(!visited);
-   }
-   return (
-    <div className="border-2 border-blue-500 p-5 rounded-xl m-4">
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  }
+
+  console.log(handleVisitedCountry);
+
+  return (
+    <div className={`border-2 border-blue-500 p-5 rounded-xl m-4 ${visited && 'bg-orange-100'}`}>
       <h3 >Name: {name?.common}</h3>
-      <img className="w-2/7" src={flags.png} alt="" />
+      <img className="w-1/2" src={flags.png} alt="" />
       <p>Population:{population}</p>
       <p>area :{area}</p>
       <p><small>Code: {cca3}</small></p>
-      <button 
+      <button className='p-1 rounded-sm cursor-pointer bg-blue-400 text-white'>Mark visited</button><br />
+      <button
         onClick={handleVisited}
-        className="bg-indigo-50 p-1 rounded-sm cursor-pointer">{visited ? 'visited':'Going'}</button>
-        {visited ? 'I have visited this country country':'I want to visit this country'}
+        className={`p-1 rounded-sm cursor-pointer ${visited ? 'bg-green-200' : 'bg-indigo-200'}`}>{visited ? 'visited' : 'Going'}</button>
+
+      {visited ? 'I have visited this country country' : 'I want to visit this country'}
     </div>
   )
-} 
+}
 
 export default Country
